997a548d228f4cd5a8b6091862825031d74aea6a
Maximilian Schanner <arthus@gfz-potsdam.de>
2025-04-01 13:26:58 +0200

    Add constants file.

---
+++ src/pyCutoff/constants.py
@@ -0,0 +1,8 @@
+REARTH = 6371.2             # Earth's radius in km
+
+# Oxgen-16
+ANUC = 16
+ZCHARGE = 8
+# Mass equivalent energy for one atomic mass unit
+EMCSQ = 0.931141            # GeV
+C = 2.99792458E5 / REARTH   # Speed of light per Earth's radius
